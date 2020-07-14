2a43c1161a018f0b34a2f5406c4c2f302ff30dce
kbn-gh <kbn.git@gmail.com>
2020-07-14 17:16:04 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+   name='criticality',
+   version='1.0',
+   description='This package used to analyze neuron data for criticality.',
+   license="",
+   long_description=long_description,
+   keywords='criticality, neuroscience, electrophysiology',
+   package_dir={'criticality': 'criticality'},
+   author='Keith Hengen, Sahara Ensley, Kiran Bhaskran-Nair,\
+           Zhengyu Ma, Ralf Wessel\
+           (Hengen Lab Washington University in St. Louis)',
+   author_email='',
+   maintainer='Kiran Bhaskaran-Nair, Sahara Ensley, Keith Hengen,\
+           (Hengen Lab Washington University in St. Louis)',
+   maintainer_email='',
+   url="https://github.com/hengenlab/criticality_hlab",
+   download_url="https://github.com/hengenlab/criticality_hlab",
+   packages=['criticality'],
+   install_requires=['ipython', 'numpy', 'matplotlib', 'seaborn', 'pandas',
+                     'joblib', 'scipy', 'sklearn', 'glob3', 'powerlaw'],
+   classifiers=[
+        'Development Status :: 1 - Pre-Alpha',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+    ],
+   scripts=[
+           ]
+)
