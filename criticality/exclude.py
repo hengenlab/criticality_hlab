79db708569ad5ff7984300ee41bd5f9bbbe803d2
kbn-gh <kbn.git@gmail.com>
2020-10-01 13:54:40 -0500

    in exclude just print subset of dKS values

--- criticality/exclude.py
+++ criticality/exclude.py
@@ -12,6 +12,7 @@ def EXCLUDE(burst, setmin, num=1, nfactor=0):
     KS = 1
     dKS = 1
     xmin = 1
+    loop_indx = 0
 
     print("burst ", burst)
     print("burst[burst > xmin] ", burst[burst > xmin])
@@ -19,7 +20,8 @@ def EXCLUDE(burst, setmin, num=1, nfactor=0):
     print("1 ", np.sqrt(np.size(burst[burst > xmin])))
     print("2 ", num/np.sqrt(np.size(burst[burst > xmin])))
     while ((KS > np.min([num/np.sqrt(np.size(burst[burst > xmin])), 0.1]))
-                and (dKS > 0.0005)):
+            and (dKS > 0.0005)):
+        loop_indx += 1
         alpha, xmin, ks, Loglike = tp.tplfit(burst, setmin, nfactor=nfactor)
         alpha = alpha[0]
         xmax = np.max(burst)
@@ -37,10 +39,12 @@ def EXCLUDE(burst, setmin, num=1, nfactor=0):
 
         KS = np.max(np.abs(cdf - fit))
         dKS = np.abs(KS_old-KS)
-        print("dKS ", dKS)
+        if not loop_indx % 200:
+            print("dKS ", dKS)
         burst = burst[burst < np.max(burst)]
         # print("len burst ", len(burst))
         burstMax = np.max(burst)
 
+    print("dKS Final ", dKS)
     burstMin = xmin
     return burstMax, burstMin, alpha
