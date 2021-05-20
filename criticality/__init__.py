e49100fee46504871ea2feb5a90a09203f922fd9
kbn-gh <kbn.git@gmail.com>
2021-05-20 15:39:03 -0500

    updates from Sahara init

--- criticality/__init__.py
+++ criticality/__init__.py
@@ -3,3 +3,8 @@ from .get_avalanches import *
 from .exclude import *
 from .pvaluenew2 import *
 from .tplfit_new import *
+
+from .crit_class import *
+from .crit_utils import *
+from .crit_hlab import *
+from .big_crit_utils import *
