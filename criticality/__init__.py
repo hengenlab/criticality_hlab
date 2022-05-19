d9da54c58799a7d9295de77aaa38215044234c51
kbn-gh <kbn.git@gmail.com>
2022-05-19 13:18:55 -0500

    removed importing crit_class crit_utils crit_hlab and big_crit_utils

--- criticality/__init__.py
+++ criticality/__init__.py
@@ -4,7 +4,7 @@ from .exclude import *
 from .pvaluenew2 import *
 from .tplfit_new import *
 
-from .crit_class import *
-from .crit_utils import *
-from .crit_hlab import *
-from .big_crit_utils import *
+# from .crit_class import *
+# from .crit_utils import *
+# from .crit_hlab import *
+# from .big_crit_utils import *
